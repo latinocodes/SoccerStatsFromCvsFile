e4d12bcb59f98ac64999b82a59e1099c894aa642
latinocodes02@gmail.com <Lara0216>
2017-10-17 21:04:42 -0400

    Initial check-in of module SoccerStats

---
+++ SoccerStats/GameResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace SoccerStats
+{
+    public class GameResult
+    {
+        public DateTime GameDate { get; set; }
+        public string TeamName { get; set; }
+        public HomeOrAway HomeORAway { get; set; }
+        public int Goals { get; set; }
+        public int GoalAttemps { get; set; }
+        public int ShotsOnGoals { get; set; }
+        public int ShotsOffGoal { get; set; }
+        public double PossessionPercent { get; set; }
+        public double ConversionRate
+        {
+            get {
+                return (double) Goals / GoalAttemps;
+            }
+        }
+
+        public enum HomeOrAway
+        {
+            Home,
+            Away
+        }
+
+    }
+}
---
+++ SoccerStats/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace SoccerStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+            var fileName = Path.Combine(directory.FullName, "data.txt");
+
+            var fileContent = ReadSoccerResults(fileName);
+
+            foreach (var cont in fileContent){
+                Console.WriteLine(cont);
+            }
+
+        }
+
+        public static string ReadFile(string fileName){
+            using(var reader = new StreamReader(fileName)){
+
+                return reader.ReadToEnd();
+            }
+
+        }
+        public static List<string[]> ReadSoccerResults(string fileName){
+            var soccerResults = new List<string[]>();
+            using(var reader = new StreamReader(fileName)){
+
+                string line = "";
+                while((line = reader.ReadLine()) != null ) {
+
+                    string[] values = line.Split(',');
+                    soccerResults.Add(values);
+                }
+
+            }
+            return soccerResults;
+        }
+    }
+}
